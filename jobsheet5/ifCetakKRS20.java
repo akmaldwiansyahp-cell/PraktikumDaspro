a75b150315d16f9acc4c7a23d0435f11b3df48a1
akmaldwiansyahp-cell <akmaldwiansyah.p@gmail.com>
2025-10-06 00:29:23 +0700

    Tugas 3 Pertanyaan 2

--- jobsheet5/ifCetakKRS20.java
+++ jobsheet5/ifCetakKRS20.java
@@ -6,17 +6,27 @@ public class ifCetakKRS20{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Apakah memiliki kartu mahasiswa? (true/false): ");
-        boolean isKartu = scanner.nextBoolean();
-        System.out.print("Apakah sudah melakukan registrasi online? (true/false): ");
-        boolean isRegistrasi = scanner.nextBoolean();
-
-        if(isKartu){
-            System.out.println("Boleh masuk");
-        }else if(isRegistrasi){
-            System.out.println("Boleh masuk");
+        System.out.print("Apakah Dosen? (true/false): ");
+        boolean isDosen = scanner.nextBoolean();
+        
+        if(isDosen){
+            System.out.println("Akses WiFi diberikan (dosen)");
         }else{
-            System.out.println("Tidak boleh masuk");
+            System.out.print("Apakah Mahasiswa? (true/false): ");
+            boolean isMahasiswa = scanner.nextBoolean();
+
+            if(isMahasiswa){
+                System.out.print("Jumlah dari sks: ");
+                int sks = scanner.nextInt();
+                if (sks >= 12) {
+                    System.out.println("Akses WiFi diberikan (mahasiswa aktif)");
+                }else{
+                    System.out.println("Akses ditolak, SKS kurang dari 12");
+                }
+            }else{
+                System.out.println("Akses ditolak");
+            }
         }
+
     }
 }
