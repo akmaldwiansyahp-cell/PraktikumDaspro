494dbaa0e77b2475b7a0beb63351f395182e3019
akmaldwiansyahp-cell <akmaldwiansyah.p@gmail.com>
2025-10-27 10:19:55 +0700

    Modifikasi Tugas 1

--- jobsheet7/Tugas1.java
+++ jobsheet7/Tugas1.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
-
 public class Tugas1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int jmlTiket, hargaTiket = 50000;
-        double totalBiaya;
+        int jmlTiket, hargaTiket = 50000, totalTiket = 0, i = 1;
+        double totalBiaya = 0;
         while(true){
-            System.out.print("Berapa banyak tiket yang dibeli: ");
+            System.out.println("Pelanggan ke-" + (i));
+            System.out.print("Berapa banyak tiket yang dibeli (Ketik 0 untuk berhenti): ");
             jmlTiket = sc.nextInt();
-
             if(jmlTiket > 4){
-                totalBiaya = jmlTiket * hargaTiket - (jmlTiket * hargaTiket * 0.1);
-                System.out.println(totalBiaya);
+                totalBiaya += jmlTiket * hargaTiket - (jmlTiket * hargaTiket * 0.1);
+                totalTiket += jmlTiket;
             }else if(jmlTiket > 10){
-                totalBiaya = jmlTiket * hargaTiket - (jmlTiket * hargaTiket * 0.15);
-                System.out.println(totalBiaya);
-            }else if(jmlTiket <= 0){
+                totalBiaya += jmlTiket * hargaTiket - (jmlTiket * hargaTiket * 0.15);
+                totalTiket += jmlTiket;
+            }else if(jmlTiket <= 4 && jmlTiket > 0){
+                totalBiaya += hargaTiket;
+                totalTiket += jmlTiket;
+            }else if(jmlTiket < 0){
                 System.out.println("Input tidak valid, coba ulangi kembali");
                 continue;
             }
+            if(jmlTiket == 0){
+                System.out.println("Program diselesaikan.");
+                break;
+            }
+            i++;
         }
-        
+        System.out.println("Total biaya untuk hari ini adalah " + totalBiaya);
+        System.out.println(totalTiket);
     }
 }
