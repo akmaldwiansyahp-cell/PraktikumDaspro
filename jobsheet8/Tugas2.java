47e360b7f3f86c7145424bc175a9659c0b8b640b
akmaldwiansyahp-cell <akmaldwiansyah.p@gmail.com>
2025-11-01 14:52:14 +0700

    Tugas 2

---
+++ jobsheet8/Tugas2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Tugas2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 1){
+                    System.out.print("*");
+                }else if(i == n){
+                    System.out.print("*");
+                }else if(1 < i && i < n){
+                    if(j == 1 || j == n){
+                        System.out.print("*");
+                    }
+                    if(1 < j && j < n){
+                    System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println("");
+        }
+    }
+}
