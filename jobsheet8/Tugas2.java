0ad99a5f4484b273d8fe7ae52d0cfb6158399a34
akmaldwiansyahp-cell <akmaldwiansyah.p@gmail.com>
2025-11-04 07:45:23 +0700

    Modifikasi Tugas 2

--- jobsheet8/Tugas2.java
+++ jobsheet8/Tugas2.java
@@ -8,14 +8,16 @@ public class Tugas2 {
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
                 if(i == 1){
-                    System.out.print("*");
+                    System.out.print(n);
                 }else if(i == n){
-                    System.out.print("*");
+                    System.out.print(n);
                 }else if(1 < i && i < n){
                     if(j == 1 || j == n){
-                        System.out.print("*");
+                        System.out.print(n);
                     }
-                    if(1 < j && j < n){
+                    if(n >= 10 && 1 < j && j<n){
+                    System.out.print("  ");
+                    }else if(1 < j && j < n){
                     System.out.print(" ");
                     }
                 }
