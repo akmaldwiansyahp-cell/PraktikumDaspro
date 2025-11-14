9fa91c6f837eab35c29185277b17eae344257bca
akmaldwiansyahp-cell <akmaldwiansyah.p@gmail.com>
2025-11-14 20:07:03 +0700

    Modifikasi Percobaan 2

--- jobsheet10/BioskopWithScanner20.java
+++ jobsheet10/BioskopWithScanner20.java
@@ -9,19 +9,43 @@ public class BioskopWithScanner20 {
 
         String[][] penonton = new String[4][2];
         while(true){
-            System.out.print("Masukkan nama: ");
-            nama = sc.nextLine();
-            System.out.print("Masukkan baris: ");
-            baris = sc.nextInt();
-            System.out.print("Masukkan kolom: ");
-            kolom = sc.nextInt();
-            sc.nextLine();
 
-            penonton[baris-1][kolom-1] = nama;
+            System.out.print("Pilih 1.Input, 2.Tampilkan, 3.Exit: ");
+            next =sc.nextLine();
 
-            System.out.print("Input penonton lain? (Y/N):");
-            next = sc.nextLine();
-            if(next.equalsIgnoreCase("n")){
+            if(next.equals("1")){
+                while(true){
+                    System.out.print("Masukkan nama: ");
+                    nama = sc.nextLine();
+                    System.out.print("Masukkan baris: ");
+                    baris = sc.nextInt();
+                    System.out.print("Masukkan kolom: ");
+                    kolom = sc.nextInt();
+                    sc.nextLine();
+
+                    if(baris > 4 || kolom > 2){
+                        System.out.println("Input baris atau kolom tidak benar, tolong coba kembali!");
+                        continue;
+                    }
+
+                    if(penonton[baris-1][kolom-1] == null || penonton[baris-1][kolom-1].equals("***")){
+                        penonton[baris-1][kolom-1] = nama;
+                        break;
+                    }else{
+                        System.out.println("\nMohon maaf, kursi yang di input telah di isi sebelumnya\n");
+                    }
+                }
+            }else if(next.equals("2")){
+                System.out.println("\t Kolom 1 \t Kolom 2");
+                for(int i = 0; i < penonton.length; i++){
+                    for(int j = 0; j < penonton[i].length; j++){
+                        if(penonton[i][j] == null){
+                            penonton[i][j] = "***";
+                        }
+                    }
+                    System.out.println("Baris " + (i+1) + "\t " + String.join("\t\t ", penonton[i]));   
+                }
+            }else if(next.equals("3")){
                 break;
             }
         }
