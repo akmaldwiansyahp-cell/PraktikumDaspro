42cbd7e18b59f0484c23b3cab6ae2bee542e617c
akmaldwiansyahp-cell <akmaldwiansyah.p@gmail.com>
2025-11-15 21:52:40 +0700

    Modifikasi Percobaan 3

--- jobsheet10/Numbers20.java
+++ jobsheet10/Numbers20.java
@@ -1,4 +1,4 @@
-import jobsheet5.nestedUjianSkripsi20;
+import java.util.Arrays;
 
 public class Numbers20 {
     public static void main(String[] args) {
@@ -7,6 +7,12 @@ public class Numbers20 {
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
 
-        
+        for(int i = 0; i < myNumbers.length; i++){
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for(int i = 0; i < myNumbers.length; i++){
+          System.out.println("Panjang baris ke-" + (i+1) + ": " + myNumbers[i].length);  
+        }
     }
 }
