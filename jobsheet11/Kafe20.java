4e024a9f8cbc707e1df49c7cd847a1d476934f31
akmaldwiansyahp-cell <akmaldwiansyah.p@gmail.com>
2025-11-29 12:39:54 +0700

    Percobaan 1

---
+++ jobsheet11/Kafe20.java
@@ -0,0 +1,19 @@
+package jobsheet11;
+
+public class Kafe20 {
+    public static void menu(){
+        System.out.println("=== Menu Resto Kafe ===");
+        System.out.println("1. Kopi hitam - Rp. 15,000");
+        System.out.println("1. Kopi hitam - Rp. 15,000");
+        System.out.println("1. Kopi hitam - Rp. 15,000");
+        System.out.println("1. Kopi hitam - Rp. 15,000");
+        System.out.println("1. Kopi hitam - Rp. 15,000");
+        System.out.println("1. Kopi hitam - Rp. 15,000");
+        System.out.println("===========================");
+        System.out.println("Silahkan pilih menu yang anda inginkan");
+    }
+
+    public static void main(String[] args) {
+        menu();
+    }
+}
