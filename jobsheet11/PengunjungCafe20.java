53e4af821568ee19276224fc0d4c914395931131
akmaldwiansyahp-cell <akmaldwiansyah.p@gmail.com>
2025-11-30 01:32:35 +0700

    Modifikasi Percobaan 4

--- jobsheet11/PengunjungCafe20.java
+++ jobsheet11/PengunjungCafe20.java
@@ -3,8 +3,8 @@ package jobsheet11;
 public class PengunjungCafe20 {
     static void daftarPengunjung(String ...namaPengunjung){
         System.out.println("Daftar nama pengunjung:");
-        for(int i = 0; i < namaPengunjung.length; i++){
-            System.out.println("- " + namaPengunjung[i]);
+        for (String a : namaPengunjung) {
+            System.out.println("- " + a);
         }
     }
 
@@ -13,4 +13,6 @@ public class PengunjungCafe20 {
         daftarPengunjung("Andi");
         daftarPengunjung("Doni", "Eti", "Fahmi", "Galih");
     }
+
+    
 }
